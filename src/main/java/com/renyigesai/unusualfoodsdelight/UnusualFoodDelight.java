8212252f0efcc9498dcfd25cd093e942015fbc08
renyigesai <2907654918@qq.com>
2026-04-02 10:39:47 +0800

    修复服务端报错2025/04/02

--- src/main/java/com/renyigesai/unusualfoodsdelight/UnusualFoodDelight.java
+++ src/main/java/com/renyigesai/unusualfoodsdelight/UnusualFoodDelight.java
@@ -1,35 +1,20 @@
 package com.renyigesai.unusualfoodsdelight;
 
 import com.mojang.logging.LogUtils;
+import com.renyigesai.unusualfoodsdelight.advancement.DragonPreservedEggTrigger;
 import com.renyigesai.unusualfoodsdelight.fluid.UdFluidTypes;
 import com.renyigesai.unusualfoodsdelight.fluid.UdFluids;
 import com.renyigesai.unusualfoodsdelight.init.*;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 @Mod(UnusualFoodDelight.MODID)
@@ -37,6 +22,8 @@ public class UnusualFoodDelight {
 
     public static final String MODID = "unusualfoodsdelight";
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DragonPreservedEggTrigger DRAGON_PRESERVED_EGG_TRIGGER = new DragonPreservedEggTrigger();
 
     public UnusualFoodDelight() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -50,6 +37,8 @@ public class UnusualFoodDelight {
         UdParticleTypes.REGISTRY.register(bus);
         UdEnchantments.ENCHANTMENTS.register(bus);
         UdGroup.CREATIVE_TABS.register(bus);
+        UdEntityTypes.ENTITY.register(bus);
+        registerTrigger();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -59,15 +48,11 @@ public class UnusualFoodDelight {
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    @SubscribeEvent
+    public void onTooltipColor(RenderTooltipEvent.Color event) {
+    }
 
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
+    public void registerTrigger(){
+        CriteriaTriggers.register(DRAGON_PRESERVED_EGG_TRIGGER);
     }
 }
